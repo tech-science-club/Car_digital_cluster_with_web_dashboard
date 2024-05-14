691d60ac2e306f1c7e14fcca42eb5f3d5c14c11a
techmadman <dimapanas87@gmail.com>
2024-05-14 23:32:14 +0200

    add plots, modification of engine class

--- Dash_project.py
+++ Dash_project.py
@@ -461,7 +461,7 @@ class Sport_style_dash_board(MDScreen):
                 self.manager.current = "dash3"
                 self.manager.transition.direction = "left"
                 self.dialog.dismiss()
-            return True
+            return True #  # S
 
 
 class Gauge_temp(MDFloatLayout):
@@ -596,57 +596,9 @@ class Engine(MDScreen):
                 self.dialog.dismiss()
             return True
 
-    def read_data(self, dt=0.1):
-        pass
-        # self.time_count += dt
-        # self.time_count = round(self.time_count)
-        # self.data = self.arduino_Data.readline()
-        # self.data = str(self.data, 'utf-8')
-        # self.data = self.data.strip('\r\n')
-        # self.data = float(self.data)
-        # self.fuel_data = self.data *180/1023
-        # self.fuel_data = round(self.fuel_data)
-
-        # self.time_count = round(self.time_count)
-        # self.get_data.append(self.fuel_data)
-        # self.get_time.append(self.time_count)
-        # print(self.get_data)
-        # print(self.get_time)
-
-    def update(self):
-        Clock.schedule_interval(self.read_data, 0.05)
-        Clock.schedule_interval(self.update_points, 0.05)
-        Clock.schedule_interval(self.update_xaxis, 0.05)
-
-    def update_xaxis(self, *args):
-        self.graph1.xmin = self.get_time[0]
-        self.graph1.xmax = self.get_time[-1]
-        self.graph2.xmin = self.get_time[0]
-        self.graph2.xmax = self.get_time[-1]
-
-    def update_points(self, *args):
-        # time.sleep(1)
-        # if len(self.get_data) == len(self.get_time):
-        self.plot.points = [(x / 100, sin(x / 10)) for x in range(1, 1000)]  # self.get_time]
-        self.plot2.points = [(x / 100, cos(x / 10)) for x in range(1, 1000)]
-        if len(self.get_time) > 10:
-            self.get_time.pop(0)
-            # self.get_data.pop(0)
-
-    # def plotting(self):
-    #    self.graph = Graph(xlabel='t', ylabel='load', x_ticks_minor=1,
-    #                           x_ticks_major=1, y_ticks_major=5,
-    #                           y_grid_label=True, x_grid_label=True, padding=5,
-    #                           x_grid=False, y_grid=False, xmin=-0, xmax=5, ymin=-1, ymax=20)
-    #    self.plot = LinePlot(color=[1, 0, 0, 1], line_width=2)
-    #
-    #    self.plot_x = self.get_time
-    #    self.plot_y = self.get_data
-    #
-    #    self.graph.add_plot(self.plot)
-    #    self.ids.engine_fuel_cons.add_widget(self.graph)
 
 
+# -----------------> Widgets of 3d Screen are bellow (Coolant temp, gearbox temp, Voltage, Oil pressure, Turbo)
 class Temp(MDFloatLayout, TouchBehavior):
     dialog = None
     name = "Temperature"
@@ -839,7 +791,7 @@ class turbo_pressure(MDFloatLayout, TouchBehavior):
         plot_box = self.dialog.content_cls
         plot_box.stop_process()
 
-
+# -------------------------> plotting of incoming data. Class is evoked from MDDialog as custom class
 class Plot_box(MDFloatLayout):
     y = NumericProperty(0)
     x = NumericProperty(0)
@@ -898,7 +850,7 @@ class Plot_box(MDFloatLayout):
         plt.xlabel('t, sec')
         plt.ylabel('ϰ, 1/R')
         plt.grid(False)
-        plt.ylim(0, 1)
+        plt.ylim(-1, 1)
         plt.style.context('dark_background')
         self.graf.canvas.draw()
         obj = FigureCanvasKivyAgg(self.graf)
